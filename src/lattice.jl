69c3a666dfa2d2ca09023107e4e4bf0e38c3ef3c
Jarvist Moore Frost <jarvist@gmail.com>
2017-10-01 00:42:48 +0100

    Modulo lattice

--- src/lattice.jl
+++ src/lattice.jl
@@ -1,6 +1,7 @@
 # Pseudea.jl, daughter of Eris
 
-export Sn,Zn,Cu,S,randCZTS,randCZTSlattice
+export Sn,Zn,Cu,S
+export randCZTS,randCZTSlattice,moduloCZTSlattice
 
 const Sn=-3
 const Zn=-2
@@ -9,3 +10,19 @@ const S=+2
 
 randCZTS() = [Cu,Sn,Zn,Zn,S,S,S,S][rand(1:8)]
 randCZTSlattice(n::Int) =  [randCZTS() for i in 1:n, j in 1:n, k in 1:n]
+
+function moduloCZTSlattice(n::Int)
+    lattice=Array{Int}(n,n,n)
+
+    for i in 1:n
+        for j in 1:n
+            for k in 1:n
+                if ((i+j+k)%2==0) lattice[i,j,k]=+2 end # paint the sulphur in; i+j+k = EVEN
+                if ((i+j+k)%4==1) lattice[i,j,k]=-2 end 
+                if ((i+j+k)%8==3) lattice[i,j,k]=-3 end 
+                if ((i+j+k)%8==7) lattice[i,j,k]=-1 end 
+            end
+        end
+    end
+    return(lattice)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,13 @@ using Base.Test
 # write your own tests here
 @test 1 == 1 
 
+println("Ten random atomic samples from CZTS...")
 for i in 1:10
     println(randCZTS()) # 
 end
 
-show(randCZTSlattice(10))
+println("A 5x5x5 CZTS random lattice...")
+show(randCZTSlattice(5))
+
+println("A 4x4x4 CZTS modulo lattice...")
+show(moduloCZTSlattice(4))
